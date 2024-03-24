846607cd76f983c6a61d86a9de36c950f217b56b
evabalu <baluchova.eva@gmail.com>
2024-03-24 13:36:17 +0100

    init commit

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,20 +1,26 @@
 import { QuestionBody } from '../QuestionBody/index';
 import { Option } from '../Option/index';
 import './style.css';
+import { useState } from 'react';
 
-export const Question = (props) => {
-  return (
+export const Question = ({text}) => {
+  const [answered, setAnswered] = useState(false);
+
+  const handleSelect = () => {
+    setAnswered(!answered);
+  }
+  
+    return (
     <div className="question">
-      <QuestionBody 
-        iconType="symbolQuestion" 
-        text={props.text}
+      <QuestionBody  type={answered ? "symbolTick"  : "symbolQuestion" }
+        text={text}
       />
       <div className="question__options">
-        <Option type="smileyStrongYes" text="Souhlasím"/>
-        <Option type="smileyYes" text="Spíše souhlasím"/>
-        <Option type="smileyNeutral" text="Nevím"/>
-        <Option type="smileyNo" text="Spíše nesouhlasím"/>
-        <Option type="smileyStrongNo" text="Nesouhlasím"/>
+        <Option onSelected={handleSelect} type="smileyStrongYes" text="Souhlasím"/>
+        <Option onSelected={handleSelect} type="smileyYes" text="Spíše souhlasím"/>
+        <Option onSelected={handleSelect} type="smileyNeutral" text="Nevím"/>
+        <Option onSelected={handleSelect} type="smileyNo" text="Spíše nesouhlasím"/>
+        <Option onSelected={handleSelect} type="smileyStrongNo" text="Nesouhlasím"/>
       </div>
     </div>
   );
